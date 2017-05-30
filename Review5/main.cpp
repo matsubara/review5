077c7e1e2a0ee8fe840e7ee74c9cb4463c2b9412
Mauricio Matsubara <mauricio@matsubara.co>
2017-05-30 18:11:38 -0300

    exercise desc

--- Review5/main.cpp
+++ Review5/main.cpp
@@ -6,6 +6,13 @@
 //  Copyright © 2017 Mauricio Matsubara. All rights reserved.
 //
 
+/*
+ 
+ 1. Create a function that sums 2 integer values and use it
+ 2. Create another function that sums 2 float values and use it
+ 
+ */
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
